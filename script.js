3e6de43b83592f05919f30cb9e03c639e42303bf
ricardo-limaco <ricardolimaco.05@gmail.com>
2021-10-28 13:31:58 +0200

    Vue image

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const vueApp = new Vue ({
   el: `#app`,
   data: {
       message: "Vue Hello",
-      mieClassi: "text-bold-red",
+      mieClassi: "text-bold",
+      image: "https://www.laramind.com/blog/wp-content/uploads/2019/05/1_nq9cdMxtdhQ0ZGL8OuSCUQ.jpg",
   }
 }); 
